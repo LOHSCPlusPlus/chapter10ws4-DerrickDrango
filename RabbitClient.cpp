fdf001261be1bb9d677f3e3786b8a06832fd70d3
DERRICKDRANGO <>
2022-04-11 04:38:32 +0000

    Finished WS 4

--- RabbitClient.cpp
+++ RabbitClient.cpp
@@ -1,7 +1,47 @@
 #include "RabbitDataBase.h"
 #include <iostream>
+#include "ReadUtils.h"
 using namespace std;
 
-int  main() {
+const int MAX_MENU_LEN = 70;
+void printMenu(char options[][MAX_MENU_LEN], int numEntries){
+  for(int index = 0; index < numEntries; index++){
+    cout << (index + 1) << ") " << options[index] << endl;
+  }
+}
+
+int main() {
+    RabbitDataBase rabbits; 
+    int option = 0;
+    char menuOptions[][MAX_MENU_LEN]={  "Print rabbit list",
+                                        "Save list to file",
+                                        "Remove rabbit",
+                                        "Add rabbit",
+                                        "Quit"};
+    rabbits.read();
+    do {
+        printMenu(menuOptions,5);
+        option = readInt("Please select an option: ");
+        switch(option) {
+            case 1:
+                rabbits.print(cout, true);
+                break;
+            case 2:
+                rabbits.save();
+                break;
+            case 3:
+                rabbits.remove();
+                break;
+            case 4:
+                rabbits.add();
+                break;
+            case 5:
+                break;
+            default:
+                cout << "Invalid option, try again" << endl;
+                break;
+        }
+    }while(option != 5);
+    
     return 0;
 }
--- RabbitDataBase.h
+++ RabbitDataBase.h
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <fstream>
 #include "Rabbit.h"
+using namespace std;
+class RabbitDataBase{
+
+  private:
+    enum {MAX_RABBITS = 50};
+    Rabbit rabbitArray[MAX_RABBITS];
+    int numRabbits;
+
+  public:
+  RabbitDataBase();
+  void read();
+  void print(ostream &out, bool printIndex);
+  void save();
+  void remove();
+  void add();
+};
